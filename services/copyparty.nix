21bb6ae97fc8bb2af09b1d909635e0ff6acde3ec
jimwa0 <jimwang314@gmail.com>
2025-12-25 16:42:23 -0500

    add dirs pt2

--- services/copyparty.nix
+++ services/copyparty.nix
@@ -27,17 +27,27 @@
   # DIRECTORIES
   systemd.tmpfiles.rules = [
     "d /srv 2775 root srv -"
+    "d /srv/docs 2775 root srv -"
+    "d /srv/projects 2775 root srv -"
+    "d /srv/games 2775 root srv -"
+    "d /srv/dumps 2775 root srv -"
+
     "d /srv/media 2775 root srv -"
     "d /srv/media/music 2775 root srv -"
     "d /srv/media/movies 2775 root srv -"
     "d /srv/media/tv 2775 root srv -"
-    "d /srv/photos 2775 root srv -"
 
-    "d /srv/docs 2775 root srv -"
-    "d /srv/uni 2775 root srv -"
-    "d /srv/projects 2775 root srv -"
-    "d /srv/games 2775 root srv -"
-    "d /srv/dumps 2775 root srv -"
+    "d /srv/images 2775 root srv -"
+    "d /srv/images/camera 2775 root srv -"
+    "d /srv/images/camera/videos 2775 root srv -"
+    "d /srv/images/memes 2775 root srv -"
+    "d /srv/images/art 2775 root srv -"
+    "d /srv/images/graphics 2775 root srv -"
+    "d /srv/images/videos 2775 root srv -"
+
     "d /srv/shared 2775 root srv -"
+    "d /srv/shared/uni 2775 root srv -"
+    "d /srv/shared/notes 2775 root srv -"
+    "d /srv/shared/music 2775 root srv -"
   ];
 }
